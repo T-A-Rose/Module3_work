30a0b32db82331381d93832d0e674646b03cd561
Tom <T.A.Rose1993@gmail.com>
2022-05-19 14:10:05 +0100

    Adjustments to the tests

--- Web_2/web-starter-level-two/spec/feature/animals_feature_spec.rb
+++ Web_2/web-starter-level-two/spec/feature/animals_feature_spec.rb
@@ -40,11 +40,12 @@ RSpec.describe "Animals Feature (Example)", type: :feature do
     fill_in "Species", with: "Dog"
     click_button "Add Animal"
 
-    click_button "Delete Cat"
+    save_and_open_page
+    click_button "Delete Dog"
 
     expect(page).to have_content "Frog"
-    expect(page).not_to have_content "Cat"
-    expect(page).to have_content "Dog"
+    expect(page).not_to have_content "Dog"
+    expect(page).to have_content "Cat"
   end
 
   it "updates animals" do
@@ -59,13 +60,32 @@ RSpec.describe "Animals Feature (Example)", type: :feature do
     fill_in "Species", with: "Dog"
     click_button "Add Animal"
 
-    click_link "Edit Cat"
+    click_link "Edit Dog"
     fill_in "Species", with: "Zebra"
     click_button "Update Animal"
 
     expect(page).to have_content "Frog"
-    expect(page).not_to have_content "Cat"
+    expect(page).not_to have_content "Dog"
     expect(page).to have_content "Zebra"
-    expect(page).to have_content "Dog"
+    expect(page).to have_content "Cat"
+  end
+
+  it "confirms the animal has been populated" do
+    visit "/animals"
+    click_link "Post your advert here!"
+    fill_in "Species", with: "Cat"
+    fill_in "Animal Name", with: "Monty"
+    fill_in "Colour", with: "Black"
+    fill_in "Size", with: "Fat"
+    fill_in "Owners Name", with: "Tom"
+    fill_in "Owners Number", with: "0199922"
+    click_button "Add Animal"
+
+    expect(page).to have_content "Cat"
+    expect(page).to have_content "Monty"
+    expect(page).to have_content "Black"
+    expect(page).to have_content "Fat"
+    expect(page).to have_content "Tom"
+    expect(page).to have_content "0199922"
   end
 end
